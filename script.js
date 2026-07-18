78c73e90279d62d4f20e390a3052a9c438208348
miguelmaynez58-gerardo <miguelmaynez58@gmail.com>
2026-07-18 05:58:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://qekczymcxxrkszoxnijx.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_JxxKjUdNSK2Pb6FbEG5vRg_fCPw4O0c";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
