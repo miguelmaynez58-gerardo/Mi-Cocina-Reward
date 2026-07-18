1e03974a4a1f7762d727d190bdae876585bdc694
miguelmaynez58-gerardo <miguelmaynez58@gmail.com>
2026-07-18 07:23:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const supabaseUrl = "https://qekczymcxxrkszoxnijx.supabase.co";
-const supabaseKey = "sb_publishable_JxxKjUdNSK2Pb6FbEG5vRg_fCPw4O0c";
-
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
-
 async function buscarPuntos() {
-  alert("Buscando...");
 
   const telefono = document.getElementById("telefono").value;
+
+  alert("Buscando teléfono: " + telefono);
 
   const { data, error } = await supabase
     .from("clientes")
     .select("*")
-    .eq("telefono", telefono)
-    .single();
+    .eq("telefono", telefono);
+
+  console.log(data);
+  console.log(error);
 
   if (error) {
     document.getElementById("resultado").innerHTML =
+      "Error: " + error.message;
+    return;
+  }
+
+  if (!data || data.length === 0) {
+    document.getElementById("resultado").innerHTML =
       "No encontramos tu cuenta.";
-    console.log(error);
     return;
   }
 
   document.getElementById("resultado").innerHTML =
-    "Hola " + data.nombre + "! Tienes " + data.puntos + " puntos.";
+    "Hola " + data[0].nombre + "! Tienes " + data[0].puntos + " puntos.";
 }
